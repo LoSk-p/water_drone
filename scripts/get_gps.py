5ee0939fe88417f0920795b297905f27ef6dbfb4
LoSk-p <alkiz.spb@gmail.com>
2020-11-13 19:33:47 +0300

    read sensor data, write gps and sensor to file

--- scripts/get_gps.py
+++ scripts/get_gps.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import NavSatFix
+from rospy_message_converter import json_message_converter
+from water_drone.msg import SensorData
 
-def callback(data):
-	gps_data = "latitude: " + str(data.latitude) + "; longitude: " + str(data.longitude) + "\n"
-	print(gps_data)
-	file.write(gps_data)
-def listener():
-	rospy.init_node("get_data", anonymous=True)
-	rospy.Subscriber("/mavros/global_position/global", NavSatFix, callback)
-	rospy.spin()
+class GetSensors:
+	def __init__(self):
+		rospy.init_node("write_file", anonymous=True)
+		self.data_gps = None
+		self.data_sensors = None
+	def callback_gps(self, data):
+		#print(data.timestamp)
+		self.data_gps = "time: " + str(data.header.stamp.secs) + "; Lat: " + str(data.latitude) + "; Lon: " + str(data.longitude)
+	def callback_sensors(self, data):
+		self.data_sensors = "; Temperature: " + str(data.temperature) + "; pH: " + str(data.pH) + "; conductivity: " + str(data.conductivity) + ";\n"
+		with open("gps+sensors", "a") as file:
+			file.write(self.data_gps + self.data_sensors)
+	def write_file(self):
+		rospy.Subscriber("/sensor_data", SensorData, self.callback_sensors)
+		rospy.Subscriber("/mavros/global_position/global", NavSatFix, self.callback_gps)
+		rospy.spin()
 if __name__ == '__main__':
-	file = open("data", 'w')
-	listener()
+	GetSensors().write_file()	
