f60ecb0fed7c55425b6fb03737b21003a5e6f0c7
LoSk-p <alki.spb@mail.ru>
2020-11-17 19:38:46 +0000

    add sending to sensors connectivity

---
+++ scripts/http_feeder.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3 
+
+import json
+import requests
+from wifi import Cell
+import subprocess
+from dataclasses import dataclass
+
+
+@dataclass()
+class Sender:
+	timestamp: float  = 0
+	def parse(self):
+		header = {"Content-type": "application/json"}
+		with open("/home/ubuntu/data/gps-sensors.json", "r") as f:
+			for data in f:
+				data = json.loads(data)
+				if int(data["time"]) > self.timestamp:
+					r = requests.post('http://connectivity.robonomics.network:65/', data=json.dumps(data), headers=header)
+					self.timestamp = int(data["time"])
+					print(data)
+
+
+	def check_connection(self):
+		while True:
+			process = subprocess.Popen("iwconfig", stdout=subprocess.PIPE)
+			networks = str(process.communicate())
+			essid = networks.find("ESSID") + 6
+			essid_end = networks.find("Mode") - 14
+			if networks[essid:essid_end] != 'off/any':
+				self.parse()
+
+Sender().check_connection()
