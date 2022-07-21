f6ae29a20de6ecc1be446aea7669d8e5aca4ff5d
tubleronchik <m.bystramovich@gmail.com>
2022-07-21 18:42:06 +0300

    refactoring

--- scripts/emulate_sensors.py
+++ scripts/emulate_sensors.py
@@ -2,30 +2,38 @@
 import time
 import rospy
 from water_drone.msg import SensorData
+import typing as tp
 
-def get_msg_data(data):
-	data_msg = SensorData()
-	data_msg.pH = data['pH']
-	data_msg.conductivity = data['conductivity']
-	data_msg.temperature = data['temperature']
-	return(data_msg)
 
-def read_data():
-	rospy.init_node("reader_sensor_data", anonymous=True)
-	pub_sensor = rospy.Publisher("sensor_data", SensorData, queue_size=10)
-	while not rospy.is_shutdown():
-		time.sleep(0.4)
-		file = open("sensor_data_saved", "r")
-		print("hell")
-		for line in file:
-			data_prev = line[2:]
-			if data_prev[0] == "<":
-				data_prev = data_prev.split('#')
-				data = {'temperature': float(data_prev[4].split(':')[1]), 'pH': float(data_prev[5].split(':')[1]), 'conductivity': float(data_prev[7].split(':')[1])}
-				data_msg = get_msg_data(data)
-				pub_sensor.publish(data_msg)
-				time.sleep(2)
-			#print(data_prev)
-		file.close()
+def get_msg_data(data: tp.Dict[str, float]) -> SensorData:
+    data_msg = SensorData()
+    data_msg.pH = data["pH"]
+    data_msg.conductivity = data["conductivity"]
+    data_msg.temperature = data["temperature"]
+    return data_msg
+
+
+def read_data() -> None:
+    rospy.init_node("reader_sensor_data", anonymous=True)
+    pub_sensor = rospy.Publisher("sensor_data", SensorData, queue_size=10)
+    while not rospy.is_shutdown():
+        time.sleep(0.4)
+        file = open("sensor_data_saved", "r")
+        print("hell")
+        for line in file:
+            data_prev = line[2:]
+            if data_prev[0] == "<":
+                data_prev = data_prev.split("#")
+                data = {
+                    "temperature": float(data_prev[4].split(":")[1]),
+                    "pH": float(data_prev[5].split(":")[1]),
+                    "conductivity": float(data_prev[7].split(":")[1]),
+                }
+                data_msg = get_msg_data(data)
+                pub_sensor.publish(data_msg)
+                time.sleep(2)
+            # print(data_prev)
+        file.close()
+
 
 read_data()
--- scripts/get_gps.py
+++ scripts/get_gps.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import NavSatFix
@@ -6,42 +5,63 @@ from water_drone.msg import SensorData
 import json
 import datetime
 
+
 class GetSensors:
-	def __init__(self):
-		rospy.init_node("write_file", anonymous=True)
-		self.data_json = {}
-		self.current_date = str(datetime.datetime.now().strftime("%Y_%m_%d"))
-		self.data_gps = None
-		self.data_sensors = None
-		self.lat = 59.0
-		self.lon = 30.0
-	def callback_gps(self, data):
-		#print(data.timestamp)
-		self.data_json['time'] = data.header.stamp.secs
-		if data.latitude == 0:
-			self.lat += 0.01
-			self.data_json['Lat'] = self.lat
-		else:
-			self.data_json['Lat'] = data.latitude
-		if data.longitude == 0:
-			self.lon += 0.01
-			self.data_json['Lon'] = self.lon
-		else:
-			self.data_json['Lon'] = data.longitude
-		self.data_gps = "time: " + str(data.header.stamp.secs) + "; Lat: " + str(data.latitude) + "; Lon: " + str(data.longitude)
-	def callback_sensors(self, data):
-		self.data_json['temp'] = data.temperature
-		self.data_json['pH'] = data.pH
-		self.data_json['cond'] = data.conductivity
-		self.data_sensors = "; Temperature: " + str(data.temperature) + "; pH: " + str(data.pH) + "; conductivity: " + str(data.conductivity) + ";\n"
-		with open("/home/pi/data/gps-sensors.json", "a") as file:
-			#file.write(self.data_gps + self.data_sensors)
-			json.dump(self.data_json, file)
-			file.write("\n")
-			print(self.data_json)
-	def write_file(self):
-		rospy.Subscriber("/sensor_data", SensorData, self.callback_sensors)
-		rospy.Subscriber("/mavros/global_position/global", NavSatFix, self.callback_gps)
-		rospy.spin()
-if __name__ == '__main__':
-	GetSensors().write_file()	
+    def __init__(self) -> None:
+        rospy.init_node("write_file", anonymous=True)
+        self.data_json = {}
+        self.current_date = str(datetime.datetime.now().strftime("%Y_%m_%d"))
+        self.data_gps = None
+        self.data_sensors = None
+        self.lat = 59.0
+        self.lon = 30.0
+
+    def callback_gps(self, data: SensorData) -> None:
+        # print(data.timestamp)
+        self.data_json["time"] = data.header.stamp.secs
+        if data.latitude == 0:
+            self.lat += 0.01
+            self.data_json["Lat"] = self.lat
+        else:
+            self.data_json["Lat"] = data.latitude
+        if data.longitude == 0:
+            self.lon += 0.01
+            self.data_json["Lon"] = self.lon
+        else:
+            self.data_json["Lon"] = data.longitude
+        self.data_gps = (
+            "time: "
+            + str(data.header.stamp.secs)
+            + "; Lat: "
+            + str(data.latitude)
+            + "; Lon: "
+            + str(data.longitude)
+        )
+
+    def callback_sensors(self, data: SensorData) -> None:
+        self.data_json["temp"] = data.temperature
+        self.data_json["pH"] = data.pH
+        self.data_json["cond"] = data.conductivity
+        self.data_sensors = (
+            "; Temperature: "
+            + str(data.temperature)
+            + "; pH: "
+            + str(data.pH)
+            + "; conductivity: "
+            + str(data.conductivity)
+            + ";\n"
+        )
+        with open("/home/pi/data/gps-sensors.json", "a") as file:
+            # file.write(self.data_gps + self.data_sensors)
+            json.dump(self.data_json, file)
+            file.write("\n")
+            print(self.data_json)
+
+    def write_file(self) -> None:
+        rospy.Subscriber("/sensor_data", SensorData, self.callback_sensors)
+        rospy.Subscriber("/mavros/global_position/global", NavSatFix, self.callback_gps)
+        rospy.spin()
+
+
+if __name__ == "__main__":
+    GetSensors().write_file()
