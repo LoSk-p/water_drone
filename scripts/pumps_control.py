5873f7e777f79e606a082ce351c67d928d7cbad7
LoSk-p <alki.spb@mail.ru>
2023-10-01 09:05:49 +0300

    add pumps test service and pump in bottle after measure

--- scripts/pumps_control.py
+++ scripts/pumps_control.py
@@ -9,6 +9,7 @@ from sensor_msgs.msg import NavSatFix
 import datetime
 from mavros_msgs.srv import SetMode, WaypointPull
 from mavros_msgs.msg import CommandCode
+from std_srvs.srv import Empty
 
 from water_drone.srv import RunPump
 from water_drone.msg import WaterLevelSensorsData, NewPump
@@ -19,6 +20,7 @@ MAIN_PUMP_OUT_DELAY = 15 # Delay after no water in low sensor
 
 class Pumps:
     def __init__(self) -> None:
+        rospy.init_node("pumps_control", anonymous=True)
         self.username = getpass.getuser()
         self.lat = None
         self.lon = None
@@ -32,10 +34,10 @@ class Pumps:
         with open(f"/home/{self.username}/catkin_ws/src/water_drone/config/config.json", "r") as f:
             self.config = json.load(f)
         self.pumps_filename = f"/home/{self.username}/data/{self.current_date}/pumps/pumps-{time.time()}.json"
-        rospy.init_node("pumps_control", anonymous=True)
         self.low_sensor_values = []
         self.up_sensor_values = []
         self.full_pumps = []
+        self.bottles_queue = [1, 4, 2, 5, 3, 6]
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.config["rpi_pins"]["sensor_up"], GPIO.IN)
         GPIO.setup(self.config["rpi_pins"]["sensor_down"], GPIO.IN)
@@ -56,10 +58,18 @@ class Pumps:
             GPIO.output(self.config["rpi_pins"]["pump5"], GPIO.HIGH)
             GPIO.setup(self.config["rpi_pins"]["pump6"], GPIO.OUT)
             GPIO.output(self.config["rpi_pins"]["pump6"], GPIO.HIGH)
-        s = rospy.Service('run_pump', RunPump, self.handle_run_pump)
+        
+        # Services
+        rospy.Service('run_pump', RunPump, self.handle_run_pump)
+        rospy.Service('test_pumps', Empty, self.test_pumps)
+
+        # Publishers
         self.publisher = rospy.Publisher("water_level_sensors", WaterLevelSensorsData, queue_size=10)
         self.new_pump_pub = rospy.Publisher("new_pump", NewPump, queue_size=10)
+
+        # Subscribers
         rospy.Subscriber("/mavros/global_position/global", NavSatFix, self.callback_gps)
+
         rospy.wait_for_service('/mavros/set_mode')
         self.mavros_setmode = rospy.ServiceProxy('/mavros/set_mode', SetMode)
         rospy.wait_for_service('/mavros/mission/pull')
@@ -91,6 +101,12 @@ class Pumps:
                 self.measure = True
             else:
                 self.measure = False
+                if self.config["bottle_after_measure"]:
+                    while self.bottles_queue[0] in self.full_pumps:
+                        self.bottles_queue.pop(0)
+                    if len(self.bottles_queue) > 0:
+                        self.pump_in(self.bottles_queue[0])
+                        self.bottles_queue.pop(0)
                 self.start_pause_mission("start")
                 self.pump_out_main()
         else:
@@ -152,6 +168,41 @@ class Pumps:
         message.pump_number = str(pump_number)
         return message
 
+    def test_pumps(self):
+        rospy.loginfo("Start test pumps")
+        rospy.loginfo("Turn on main pump in")
+        GPIO.output(self.config["rpi_pins"]["pump_in_main"], GPIO.LOW)
+        time.sleep(4)
+        GPIO.output(self.config["rpi_pins"]["pump_in_main"], GPIO.HIGH)
+        rospy.loginfo("Turn on main pump out")
+        GPIO.output(self.config["rpi_pins"]["pump_out_main"], GPIO.LOW)
+        time.sleep(4)
+        GPIO.output(self.config["rpi_pins"]["pump_out_main"], GPIO.HIGH)
+        rospy.loginfo("Turn on pump 1")
+        GPIO.output(self.config["rpi_pins"]["pump1"], GPIO.LOW)
+        time.sleep(4)
+        GPIO.output(self.config["rpi_pins"]["pump1"], GPIO.HIGH)
+        rospy.loginfo("Turn on pump 2")
+        GPIO.output(self.config["rpi_pins"]["pump2"], GPIO.LOW)
+        time.sleep(4)
+        GPIO.output(self.config["rpi_pins"]["pump2"], GPIO.HIGH)
+        rospy.loginfo("Turn on pump 3")
+        GPIO.output(self.config["rpi_pins"]["pump3"], GPIO.LOW)
+        time.sleep(4)
+        GPIO.output(self.config["rpi_pins"]["pump3"], GPIO.HIGH)
+        rospy.loginfo("Turn on pump 4")
+        GPIO.output(self.config["rpi_pins"]["pump4"], GPIO.LOW)
+        time.sleep(4)
+        GPIO.output(self.config["rpi_pins"]["pump4"], GPIO.HIGH)
+        rospy.loginfo("Turn on pump 5")
+        GPIO.output(self.config["rpi_pins"]["pump5"], GPIO.LOW)
+        time.sleep(4)
+        GPIO.output(self.config["rpi_pins"]["pump5"], GPIO.HIGH)
+        rospy.loginfo("Turn on pump 6")
+        GPIO.output(self.config["rpi_pins"]["pump6"], GPIO.LOW)
+        time.sleep(4)
+        GPIO.output(self.config["rpi_pins"]["pump6"], GPIO.HIGH)
+
     def pump_in(self, number_of_pump: int):
         if not self.config["extra_pumps"]:
             rospy.logerr("The node was initialised without extra pumps")
@@ -189,13 +240,13 @@ class Pumps:
             data.up_sensor = GPIO.input(self.config["rpi_pins"]["sensor_up"])
             self.publisher.publish(data)
             self.up_sensor_values.append(GPIO.input(self.config["rpi_pins"]["sensor_up"]))
-            if len(self.up_sensor_values) >= 5:
+            if len(self.up_sensor_values) >= 6:
                 self.up_sensor_values.pop(0)
             self.low_sensor_values.append(GPIO.input(self.config["rpi_pins"]["sensor_down"]))
-            if len(self.low_sensor_values) >= 5:
+            if len(self.low_sensor_values) >= 6:
                 self.low_sensor_values.pop(0)
             if self.measure:
-                if data.up_sensor:
+                if all(self.up_sensor_values):
                     time_to_pump_in_mes = time.time()
                     rospy.loginfo("Pump in water while measure")
                     GPIO.output(self.config["rpi_pins"]["pump_in_main"], GPIO.LOW)
